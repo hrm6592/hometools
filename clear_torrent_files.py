5d968dcfa0d18eccdd5a25e9f68a595a8fcec5d7
hrm <hrm.html@gmail.com>
2022-02-10 13:14:51 +0900

    - Add syslog logging

--- clear_torrent_files.py
+++ clear_torrent_files.py
@@ -2,6 +2,7 @@
 
 import argparse
 import re
+import syslog
 from os import chdir
 from pathlib import Path
 
@@ -9,6 +10,7 @@ from torrentool.api import Torrent
 
 
 def main():
+    syslog.openlog(logoption=syslog.LOG_NDELAY, facility=syslog.LOG_LOCAL2)
     args = argparse.ArgumentParser(description="Clear operation finished torrent file.")
     args.add_argument(
         "-d",
@@ -29,11 +31,13 @@ def main():
     entries = Path(args.homedir)
     if not entries.is_dir:
         raise NotADirectoryError
+    syslog.syslog(syslog.LOG_INFO, "TorrentDir: {}".format(args.homedir))
     chdir(args.homedir)
 
     resumes = Path(args.resumedir)
     if not resumes.is_dir:
         raise NotADirectoryError
+    syslog.syslog(syslog.LOG_INFO, "ResumeDir: {}".format(args.resumedir))
     resume_files = list(resumes.iterdir())
 
     target_suffix = re.compile(r".torrent$")
@@ -42,8 +46,11 @@ def main():
             continue
 
         torrent = Torrent.from_file(e.name)
+        # Some invalid torrent file does not have any information.
         if torrent.name is None:
-            print("Torrent {} does not have name!".format(e.name))
+            syslog.syslog(
+                syslog.LOG_WARNING, "Torrent {} does not have name!".format(e.name)
+            )
             e.unlink()
             continue
 
@@ -51,8 +58,13 @@ def main():
             if re.search("^{}".format(re.escape(str(torrent.name))), f.name):
                 break
         else:
+            syslog.syslog(syslog.LOG_DEBUG, "Unlink {}".format(e.name))
             e.unlink()
             continue
+
+    syslog.syslog(syslog.LOG_INFO, "Torrent file check done. exit.")
+    syslog.closelog()
+    return 0
 
 
 if __name__ == "__main__":
