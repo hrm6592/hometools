f8631c4382a0b7b7b719e817d138aa72f69bd898
hrm <hrm.html@gmail.com>
2022-03-19 14:27:28 +0900

    - Add intermediate link page parser.

--- torrent_index_parser.py
+++ torrent_index_parser.py
@@ -54,12 +54,60 @@ class IndexParser(HTMLParser):
             return
 
 
-class DLLinkParser(HTMLParser):
-    def __init__(self, *, convert_charrefs: bool = ...) -> None:
+class RedirectorParser(HTMLParser):
+    def __init__(self, dllink: str = "", convert_charrefs: bool = ...) -> None:
         super().__init__(convert_charrefs=convert_charrefs)
         self.flag_found_torrent: bool = False
+        self.dllink: str = dllink
 
-    def handle_starttag(self, tag: str, attrs: List[tuple[str, Optional[str]]]):
+    def handle_starttag(
+        self,
+        tag: str,
+        attrs: List[tuple[str, Optional[str]]],
+    ):
+        d: Dict[str, str | None] = dict(attrs)
+        if tag == "div" and d.get("class", "") == "main":
+            self.flag_found_torrent = True
+            return
+        elif (
+            tag == "a"
+            and self.flag_found_torrent == True
+            and d.get("href", "") is not None
+        ):
+            headers = {
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+                "Accept-Encoding": "gzip, deflate",
+                "Accept-Language": "ja,en;q=0.5",
+                "Connection": "keep-alive",
+                "DNT": "1",
+                "Host": "r.jtl.re",
+                "Referer": "http://javtorrent.re/",
+                "Upgrade-Insecure-Requests": "1",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0",
+            }
+            href = d.get("href", "")
+            if href is not None:
+                href = "http://r.jtl.re" + href
+                print("href: {}".format(href))
+                r: requests.Response = requests.post(href, headers=headers)
+                dlp = DLLinkParser(referer=str(href))
+                dlp.feed(r.text)
+                return
+
+
+class DLLinkParser(HTMLParser):
+    def __init__(
+        self, referer: str = "http://r.jtl.re/", convert_charrefs: bool = ...
+    ) -> None:
+        super().__init__(convert_charrefs=convert_charrefs)
+        self.flag_found_torrent: bool = False
+        self.referer: str = referer
+
+    def handle_starttag(
+        self,
+        tag: str,
+        attrs: List[tuple[str, Optional[str]]],
+    ):
         d: Dict[str, str | None] = dict(attrs)
         if tag == "div" and d.get("class", "") == "main":
             self.flag_found_torrent = True
@@ -76,7 +124,7 @@ class DLLinkParser(HTMLParser):
                 "Connection": "keep-alive",
                 "DNT": "1",
                 "Host": "jtl.re",
-                "Referer": "http://r.jtl.re/",
+                "Referer": self.referer,
                 "Upgrade-Insecure-Requests": "1",
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0",
             }
