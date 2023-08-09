b32d11161e4c828bc05c7a3701e933ae8e5d827e
HRM <hrm_html@hotmail.com>
2023-08-09 16:10:27 +0900

    - fixed CRLF lines.

--- getNewRelease.sh
+++ getNewRelease.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
 mastodon=${HOME}/.mastodon
-now=$(cat ${mastodon} 2> /dev/null)
+now=$(cat "${mastodon}" 2> /dev/null)
 new=$(curl -s https://github.com/tootsuite/mastodon/releases | fgrep css-truncate-target | head -1 | sed -e 's/ //g; s/<.[^>]*>//g')
-if [ "x${new}" != "x" -a "${new}" != "${now}" ]; then
-     echo ${new} | tee ${mastodon} | whisper -b -c '#admin@pkan'
+if [ "x${new}" != "x" ] && [ "${new}" != "${now}" ]; then
+     echo "${new}" | tee "${mastodon}" | whisper -b -c '#admin@pkan'
 fi
