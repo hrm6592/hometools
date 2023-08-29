d94a0a55fdeee89bfd91a0f99bef77178d0fe93d
HRM <hrm_html@hotmail.com>
2023-08-30 05:34:41 +0900

    - syslog pylint error fixes.

--- clear_torrent_files.py
+++ clear_torrent_files.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
 
 import argparse
+import os
 import re
 import syslog
-from os import chdir
 from pathlib import Path
 
 from torrentool.api import Torrent
 
 
 def main():
-    syslog.openlog(logoption=syslog.LOG_NDELAY, facility=syslog.LOG_LOCAL2)
-    args = argparse.ArgumentParser(description="Clear operation finished torrent file.")
+    syslog.openlog(logoption=syslog.LOG_NDELAY,  # type: ignore
+                   facility=syslog.LOG_LOCAL2)  # type: ignore
+    args = argparse.ArgumentParser(
+        description="Clear operation finished torrent file.")
     args.add_argument(
         "-d",
         "--homedir",
@@ -29,15 +31,17 @@ def main():
     args = args.parse_args()
 
     entries = Path(args.homedir)
-    if not entries.is_dir:
+    if not os.path.exists(entries):
         raise NotADirectoryError
-    syslog.syslog(syslog.LOG_INFO, "TorrentDir: {}".format(args.homedir))
-    chdir(args.homedir)
+    syslog.syslog(syslog.LOG_INFO, "TorrentDir: {}".format(  # type: ignore
+        args.homedir))
+    os.chdir(args.homedir)
 
     resumes = Path(args.resumedir)
-    if not resumes.is_dir:
+    if not resumes.is_dir():
         raise NotADirectoryError
-    syslog.syslog(syslog.LOG_INFO, "ResumeDir: {}".format(args.resumedir))
+    syslog.syslog(syslog.LOG_INFO, "ResumeDir: {}".format(  # type: ignore
+        args.resumedir))
     resume_files = list(resumes.iterdir())
 
     target_suffix = re.compile(r".torrent$")
@@ -48,8 +52,9 @@ def main():
         torrent = Torrent.from_file(e.name)
         # Some invalid torrent file does not have any information.
         if torrent.name is None:
-            syslog.syslog(
-                syslog.LOG_WARNING, "Torrent {} does not have name!".format(e.name)
+            syslog.syslog(  # type: ignore
+                syslog.LOG_WARNING, "Torrent {} does not have name!".format(  # type: ignore
+                    e.name)
             )
             e.unlink()
             continue
@@ -58,12 +63,13 @@ def main():
             if re.search("^{}".format(re.escape(str(torrent.name))), f.name):
                 break
         else:
-            syslog.syslog(syslog.LOG_DEBUG, "Unlink {}".format(e.name))
+            syslog.syslog(syslog.LOG_DEBUG, "Unlink {}".format(  # type: ignore
+                e.name))
             e.unlink()
             continue
 
     syslog.syslog(syslog.LOG_INFO, "Torrent file check done. exit.")
-    syslog.closelog()
+    syslog.closelog()  # type: ignore
     return 0
 
 
